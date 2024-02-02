430fbbd0382eaad5c098a333db4027c18395971d
Bhavana Singh <100597557+Bhavana-Somvanshi@users.noreply.github.com>
2024-02-02 06:19:10 +0000

    initial commit

---
+++ first_react_app/src/Message.tsx
@@ -0,0 +1,12 @@
+//PascalCasing
+function Message(){
+      //jsx:JavaScript + XML
+      const name="rishu";   
+      if(name){
+        return <h1>hello {name}</h1>
+      }  
+    return <h1>Namaste Bhavana</h1>
+
+}
+
+export default Message;
